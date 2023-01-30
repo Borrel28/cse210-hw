6757c6defa5c8d586d114888c0e657ee8f02e285
Borrel28 <113734040+Borrel28@users.noreply.github.com>
2023-01-30 23:21:19 +0000

    Update Program.cs

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -3,6 +3,8 @@ using System;
 class Program
 {
     static void Main(string[] args)
+        job1._jobTitle = "Software Engineer";
+        job1._Company = "Microsoft";
     {
         Console.WriteLine("Hello Learning02 World!");
     }
