d01cbc8e4f25bef963024a4f1dc93cec4a3ce465
Borrel28 <113734040+Borrel28@users.noreply.github.com>
2023-01-30 23:49:46 +0000

    Update Job.cs

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -2,10 +2,16 @@ using systerm
 
 public class Job
 {
+        // Keeps track of the company, job title, start year, and end year.
         public string _Company = "Microsoft";
         public string _JobTitle = "Software Engineer";
-        public string _StartYear = "2019"; 
-        public string _EndYear = "2022";
+        public int _StartYear = "2019"; 
+        public int _EndYear = "2022";
+        
+        public void Display()
+         {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+         }
 
 
 }
