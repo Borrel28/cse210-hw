5c5f221acb4ae198935a25516d6de4be32dbdf5a
Borrel28 <113734040+Borrel28@users.noreply.github.com>
2023-01-30 23:05:05 +0000

    Create Job.cs
    week02

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,11 @@
+using systerm
+
+public class Job
+{
+        public string _Company = "Microsoft";
+        public string _JobTitle = "Software Engineer";
+        public string _StartYear = "2019"; 
+        public string _EndYear = "2022";
+
+
+}
