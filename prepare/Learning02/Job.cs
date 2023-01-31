341ad79e5a9ca6aad2f90c405d523b0fb918cf57
Borrel28 <113734040+Borrel28@users.noreply.github.com>
2023-01-31 00:16:34 +0000

    Update Job.cs

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -1,17 +1,26 @@
 using systerm
 
 public class Job
+//My program contain two classes one for a Job and one for the Resume itself, as follows:
+public class Job
 {
         // Keeps track of the company, job title, start year, and end year.
-        public string _Company = "Microsoft";
-        public string _JobTitle = "Software Engineer";
-        public int _StartYear = "2019"; 
-        public int _EndYear = "2022";
-        
-        public void Display()
-         {
-        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
-         }
+        job1.Display();
+        job1 ._JobTitle = "Software Engineer";
+        job1 ._Company = "Microsoft";
+        job1 ._StartYear = "2019"; 
+        job1 ._EndYear = "2022";
 
+        job2.Display();
+        job2 ._JobTitle = "Techcnicien Engineer";
+        job2 ._Company = "Autocad";
+        job2 ._StartYear = "2006"; 
+        job2 ._EndYear = "2022";
+
+        // A method that displays the person's full name <family name, given name>.
+        DisplayJobDetails()
+        {
+            Console.WriteLine($"{_JobTitle}, {_Company}, {_StartYear}, {_EndYear}");
+        }
 
 }
