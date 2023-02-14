354e4bcafaa3c787182ef608c5a411e732ba267b
Borrel28 <113734040+Borrel28@users.noreply.github.com>
2023-02-14 01:41:39 +0000

    Update Program.cs

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -1,9 +1,54 @@
-using System;
+using system
 
-class Program
+class program
 {
-    static void Main(string[] args)
+    static void Main(string[]args)
     {
-        Console.WriteLine("Hello Develop05 World!");
+        AllPoints    allPoint = new AllPoints();
+        int        userMainMenuSelection = 0;
+
+        while (userMainMenuSelection !=10)
+        {
+            Menu menu = new Menu();
+            menu.DisplayPoints(allPoints.getTotalPoints());
+            menu.DisplayMainMenu();
+            userMainMenuSelection = menu.DisplayMainMenu();
+
+            switch (userMainMenuSelection)
+            {
+                
+                case 1:
+                    point = new SimplePoint();
+                    point.DisplayGetPointName();
+                    point.DisplayGetDescpition();
+                    point.DisplayGetPointName();
+                    break;
+
+            }
+            if (point := null)
+            {
+                AllPoints.addPoint(point);
+
+            }
+            break;
+        case 2:
+            allPoints.DisplayPoints();
+            break;
+        case 3:
+            allPoints.SavePoints();
+            break;
+        case 4:
+            allPoints.LoadPoints();
+            break;
+{
+        Console.WriteLine("{allPoints}!");
+}
+        }
     }
 }
+
+
+
+
+
+
